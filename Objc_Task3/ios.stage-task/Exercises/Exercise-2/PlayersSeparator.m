61bac60ef7c1f3ac72e83e6bebbed0e3c1dfe34b
achuyanov <55102485+achuyanov@users.noreply.github.com>
2021-05-24 21:41:38 +0300

    feat: task 2

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -2,8 +2,22 @@
 
 @implementation PlayersSeparator
 
-- (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+- (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)arr {
+    int len = (int)arr.count;
+    int ret = 0;
+    if (arr && len>=3) {
+    double ai, aj, ak;
+    for (int i=0; i<len-2; i++) {
+        for (int j=i+1; j<len-1; j++) {
+            for (int k=j+1; k<len; k++) {
+                ai = arr[i].doubleValue;
+                aj = arr[j].doubleValue;
+                ak = arr[k].doubleValue;
+                if (ai<aj && aj<ak) ret++;
+                if (ai>aj && aj>ak) ret++;
+            }}};
+    }
+    return ret;
 }
 
 @end
