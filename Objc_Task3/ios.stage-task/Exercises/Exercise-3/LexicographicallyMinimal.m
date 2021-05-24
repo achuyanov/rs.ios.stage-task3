718bbf772b693492590ac4feba2954ab48994dc3
achuyanov <55102485+achuyanov@users.noreply.github.com>
2021-05-24 23:05:25 +0300

    feat task 3

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,7 +9,24 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
-    return nil;
+    
+    NSMutableString *st1 = [string1 mutableCopy];
+    NSMutableString *st2 = [string2 mutableCopy];
+    NSMutableString *ret = [NSMutableString new];
+    
+    while (st1.length!=0 || st2.length!=0) {
+        
+        if (st1.length==0) {[ret appendString:st2];break;}
+        if (st2.length==0) {[ret appendString:st1];break;}
+
+        if ([st1 characterAtIndex:0] <= [st2 characterAtIndex:0]) {
+            [ret appendString:[st1 substringToIndex:1]];
+            st1 = [[st1 substringFromIndex:1] mutableCopy];}
+        else {
+            [ret appendString:[st2 substringToIndex:1]];
+            st2 = [[st2 substringFromIndex:1] mutableCopy];}
+    }
+    return ret;
 }
 
 @end
